6a900e730a45ad2af51aff44ec34596b5203f8a8
RohMoon <rs.moony@gmail.com>
2023-01-23 19:09:15 +0900

    feat(Chapter1.md UserDaoTest.java): JDBC를 이용한 UserDaoTest 추가

---
+++ src/test/java/com/tobySpring/chapter1/UserDaoTest.java
@@ -0,0 +1,34 @@
+package com.tobySpring.chapter1;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    void add() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("blackShip");
+        user.setName("흑선");
+        user.setPassword("move");
+
+        userDao.add(user);
+        
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+    }
+
+    @Test
+    void get() {
+    }
+}
